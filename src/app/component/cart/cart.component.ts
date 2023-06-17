e6fef22ff74239b080ab5535534be1f25a372f5a
Ayushi Tanwar <116892072+AyushiTanwar@users.noreply.github.com>
2023-06-17 11:32:17 +0530

    changes4

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  public product:any=[];
+  public grandTotal:number=0
+
+  constructor(private cart:CartService){}
+
+  ngOnInit():void{
+    this.cart.getProduct().subscribe(res=>{
+      this.product=res;
+      this.grandTotal=this.cart.getTotalPrice()
+    })
+  }
 }
